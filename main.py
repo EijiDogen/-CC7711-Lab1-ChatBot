23749b42da673e9766bab7a9bfff617bdba1dd84
EijiDogen <80010076+EijiDogen@users.noreply.github.com>
2022-08-21 20:01:51 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from chatbot import ChatBot
+import os
+
 myChatBot = ChatBot()
 #apenas carregar um modelo pronto
-myChatBot.loadModel()
+#myChatBot.loadModel()
 
 #criar o modelo
-#myChatBot.createModel()
+myChatBot.createModel()
 
+os.system('cls')
 print("Bem-vindo ao Chatbot FEI Intercâmbio.")
 
 pergunta = input("Como posso ajudar?\n")
@@ -14,13 +17,19 @@ resposta, intencao = myChatBot.chatbot_response(pergunta)
 #debugging print
 #print(resposta + "   ["+intencao[0]['intent']+"]")
 
+os.system('cls')
+
 #normal print
 print(resposta+"\n")
 
 while (intencao[0]['intent']!="despedida"):
-    pergunta = input("Possui outra dúvida?")
+    
+    pergunta = input("Possui outra dúvida?\n")
+    
+    os.system('cls')
     resposta, intencao = myChatBot.chatbot_response(pergunta)
 
     print(resposta+"\n")
 
 print("Foi um prazer atendê-lo")
+input()
